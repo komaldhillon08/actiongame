0c91940622989acc7123d3ce34984a183d6a0739
Sandeep <your-email@example.com>
2025-03-24 16:25:55 +0530

    add new code

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 const scoreCont = document.querySelector(".scoreCont"); // Select the element with the ID 'scoreContainer'
-score = 0 ;
-cross = true ; 
+score = 0;
+cross = true;
 let gameoveraudio = new Audio('./music/gameover.mp3');
 
-let music = new Audio("./music/music.mp3") ;
+let music = new Audio("./music/music.mp3");
 
 
 setTimeout(() => {
     music.play();
-},1000);
+}, 1000);
 
 document.onkeydown = function (e) {
     console.log("key code is ", e.keyCode);
@@ -23,15 +23,15 @@ document.onkeydown = function (e) {
     if (e.keyCode == 39) {
         dino = document.querySelector(".dino");
         dinox = parseInt(window.getComputedStyle(dino, null).getPropertyValue("left"));
-        dino.style.left = dinox + 100 + "px" ; 
+        dino.style.left = dinox + 100 + "px";
     }
     if (e.keyCode == 37) {
         dino = document.querySelector(".dino");
         dinox = parseInt(window.getComputedStyle(dino, null).getPropertyValue("left"));
-        dino.style.left = (dinox - 100) + "px" ; 
+        dino.style.left = (dinox - 100) + "px";
     }
 }
-// game over
+//  game over
 setInterval(() => {
     dino = document.querySelector(".dino");
     obstacle = document.querySelector(".obstacle");
@@ -46,25 +46,33 @@ setInterval(() => {
     offsetx = Math.abs(dx - ox);
     offsety = Math.abs(dy - oy);
 
-  //  console.log(offsetx, offsety);
+    //  console.log(offsetx, offsety);
 
     if (offsetx < 75 && offsety < 52) {
-// game over 
+        // game over 
         gameover.style.visibility = 'visible'; // Correct visibility value
         obstacle.classList.remove('obstacleAni');
-    }
-    else if(offsetx < 145 && cross){
-        score += 1;
-        updateScore(score) ;
-        cross = false ; 
+        music.pause(); 
+        gameoveraudio.play();
+
         setTimeout(() => {
-            cross = true ; 
+            gameoveraudio.pause();
         }, 1000);
-        
+
+    }
+
+    else if (offsetx < 145 && cross) {
+        score += 1;
+        updateScore(score);
+        cross = false;
         setTimeout(() => {
-            anydur =  parseFloat(window.getComputedStyle(dino, null).getPropertyValue('animation-duration'));
-            newDur = anydur - 0.1 ;
-            obstacle.style.animationDuration = newDur + " s "  ; 
+            cross = true;
+        }, 1000);
+
+        setTimeout(() => {
+            anydur = parseFloat(window.getComputedStyle(dino, null).getPropertyValue('animation-duration'));
+            newDur = anydur - 0.1;
+            obstacle.style.animationDuration = newDur + " s ";
         }, 1000);
     }
 
@@ -76,7 +84,7 @@ setInterval(() => {
 function updateScore(score) {
     scoreCont.innerText = "Your Score : " + score
     scoreCont.style.transition = 'all 1s ease-in-out';
-   
+
 }
 
 
